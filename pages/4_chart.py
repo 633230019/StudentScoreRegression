91c628d360f73a3494d90bceedc3954fd8b429a8
633230019 <144575516+633230019@users.noreply.github.com>
2023-10-08 10:58:27 +0700

    .commit

--- pages/4_chart.py
+++ pages/4_chart.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.header("Show Data Index Price")
-df=pd.read_csv("./data/stock_index_price.csv")
+df=pd.read_csv("./data/score.csv")
 st.write(df.head(10))
 
 st.header("Show Chart")
 st.line_chart(
-   df, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+   df, x="Hours", y=["Scores"], color=["#FF0000", "#0000FF"]  # Optional
 )
 
--- pages/5_page5.py
+++ pages/5_page5.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score,mean_squared_error
 
-st.title("Index Price Prediction")
-st.header("Index Price Prediction from NPRU")
+st.title("Student Score Prediction")
+st.header("Student Score Prediction from NPRU")
 
 df=pd.read_csv('./data/stock_index_price.csv')
 st.write(df.head(10))
@@ -15,11 +15,11 @@ st.write(df.head(10))
 #st.line_chart(df)
 #st.line_chart(df, x="interest_rate", y="unemployment_rate", color="stock_index_price")
 st.line_chart(
-   df, x="interest_rate", y=["unemployment_rate", "stock_index_price"], color=["#FF0000", "#0000FF"]  # Optional
+   df, x="Hours", y=["Scores"], color=["#FF0000", "#0000FF"]  # Optional
 )
 
-x=df[['interest_rate','unemployment_rate']]
-y=df['stock_index_price']
+x=df[['Hours']]
+y=df['Scores']
 pf=PolynomialFeatures(degree=3)
 x_poly=pf.fit_transform(x)
 
@@ -27,11 +27,10 @@ x_train,x_test,y_train,y_test =train_test_split(x_poly,y,random_state=0)
 
 modelRegress=LinearRegression()
 modelRegress.fit(x_train,y_train)
-x1=st.number_input("กรุณาป้อนข้อมูล interest_rate:")
-x2=st.number_input("กรุณาป้อนข้อมูล unemployment_rate:")
+x1=st.number_input("กรุณาป้อนข้อมูล ชั่วโมงการเรียน:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    x_input=[[x1,x2]]
+    x_input=[[x1]]
     y_predict=modelRegress.predict(pf.fit_transform(x_input))
     st.write(y_predict)
     st.button("ไม่พยากรณ์ข้อมูล")
