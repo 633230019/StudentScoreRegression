c7343aa6e23342594cf8d398bb1a9435697abb87
633230019 <144575516+633230019@users.noreply.github.com>
2023-10-08 11:01:22 +0700

    .commit

--- pages/5_page5.py
+++ pages/5_page5.py
@@ -15,10 +15,10 @@ st.write(df.head(10))
 #st.line_chart(df)
 #st.line_chart(df, x="interest_rate", y="unemployment_rate", color="stock_index_price")
 st.line_chart(
-   df, x="Hours", y=["Scores"], color=["#FF0000", "#0000FF"]  # Optional
+   df, x="Hours", y="Scores", color=["#FF0000", "#0000FF"]  # Optional
 )
 
-x=df[['Hours']]
+x=df['Hours']
 y=df['Scores']
 pf=PolynomialFeatures(degree=3)
 x_poly=pf.fit_transform(x)
@@ -30,7 +30,7 @@ modelRegress.fit(x_train,y_train)
 x1=st.number_input("กรุณาป้อนข้อมูล ชั่วโมงการเรียน:")
 
 if st.button("พยากรณ์ข้อมูล"):
-    x_input=[[x1]]
+    x_input=[x1]
     y_predict=modelRegress.predict(pf.fit_transform(x_input))
     st.write(y_predict)
     st.button("ไม่พยากรณ์ข้อมูล")
