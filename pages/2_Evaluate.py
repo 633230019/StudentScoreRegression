50c343a459ac5c555b48192f44e92e2ce43c63f8
633230019 <144575516+633230019@users.noreply.github.com>
2023-10-28 10:47:16 +0700

    Update 2_Evaluate.py

--- pages/2_Evaluate.py
+++ pages/2_Evaluate.py
@@ -64,6 +64,6 @@ y_predict=modelRegress.predict(x_test)
 df_predict_compare = pd.DataFrame({'y_test': y_test, 'y_predict': y_predict})
 st.header("ตารางเทียบผลการพยากรณ์คะแนนจากโมเดล 90/10")
 st.write(df_predict_compare)
-st.scatter_chart(
+st.line_chart(
    df_predict_compare
 )
